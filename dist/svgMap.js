ef2ac6206783c64d5580d0711d7f192f7e8de364
Stephan Wagner <stephanwagner.me@gmail.com>
2019-05-14 19:18:14 +0200

    Update dist files

--- dist/svgMap.js
+++ dist/svgMap.js
@@ -1,5 +1,5 @@
 /*! svgMap v1.0.0 | https://github.com/StephanWagner/svgMap | MIT License | Copyright Stephan Wagner | https://stephanwagner.me */
-/*! svg-pan-zoom v3.6.0 | https://github.com/ariutta/svg-pan-zoom | BSD 2-Clause "Simplified" License | Copyright Andrea Leofreddi <a.leofreddi@itcharm.com> */
+/*! svg-pan-zoom | https://github.com/ariutta/svg-pan-zoom | BSD 2-Clause "Simplified" License | Copyright Andrea Leofreddi <a.leofreddi@itcharm.com> */
 // svg-pan-zoom v3.6.0
 // https://github.com/ariutta/svg-pan-zoom
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
@@ -1933,10 +1933,6 @@ svgMap.prototype.init = function (options) {
     colorMin: '#FFE5D9',
     colorNoData: '#E2E2E2',
 
-    // Data thresholds
-    thresholdMax: null,
-    thresholdMin: null,
-
     // The flag type can be 'image' or 'emoji'
     flagType: 'emoji',
 
@@ -1968,6 +1964,7 @@ svgMap.prototype.init = function (options) {
   // Apply map data
   this.applyData(this.options.data);
 };
+
 svgMap.prototype.countries = {
   AF: 'Afghanistan',
   AX: 'Åland Islands',
