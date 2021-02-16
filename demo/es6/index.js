f2bbc385abda7e3f1f18fe80d65a84daaa387b9a
Stephan Wagner <stephanwagner.me@gmail.com>
2021-02-16 16:20:14 +0100

    Fix and add es6 demos

--- demo/es6/index.js
+++ demo/es6/index.js
@@ -8,3 +8,26 @@ new svgMap({
   targetElementID: 'svgMapGPD',
   data: dataGDP
 });
+
+// Demo population
+import dataPopulation from './data/population';
+
+new svgMap({
+  targetElementID: 'svgMapPopulation',
+  data: dataPopulation,
+  flagType: 'emoji'
+});
+
+// Demo person height in German
+import dataPersonHeight from './data/personHeight';
+import countriesDE from './local/countriesDE';
+
+new svgMap({
+  targetElementID: 'svgMapPersonHeight',
+  countryNames: countriesDE,
+  data: dataPersonHeight,
+  colorMin: '#FFF0F9',
+  colorMax: '#730B62',
+  hideFlag: true,
+  noDataText: 'Keine Daten vorhanden'
+});
