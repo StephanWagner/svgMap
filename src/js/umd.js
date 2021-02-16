ad98b84af4cac28fa3851e961d8b1d78f7558331
Stephan Wagner <stephanwagner.me@gmail.com>
2021-02-16 16:11:10 +0100

    Fix umd wrapper

--- src/js/umd.js
+++ src/js/umd.js
@@ -1,12 +1,12 @@
 (function (root, factory) {
   if (typeof define === 'function' && define.amd) {
-    define(['svgPanZoom'], function (svgPanZoom) {
+    define(['svg-pan-zoom'], function (svgPanZoom) {
       return (root.svgMap = factory(svgPanZoom));
     });
   } else if (typeof module === 'object' && module.exports) {
-    module.exports = root.svgMap = factory(require('svgPanZoom'));
+    module.exports = (root.svgMap = factory(require('svg-pan-zoom')));
   } else {
-    root.svgMap = factory(svgPanZoom);
+    root.svgMap = factory(root.svgPanZoom);
   }
 })(this, function (svgPanZoom) {
   var svgMap = svgMapWrapper(svgPanZoom);
