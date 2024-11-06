1752db7cf865ac322277f7fdd731ddf8c11b5bfa
Miriam Müller <74644501+m1rm@users.noreply.github.com>
2024-11-06 03:32:34 +0100

    Feature: allow other color formats for colorMax, colorMin & colorEmpty (#128)
    * feature: add functionality to use other color formats than only full hex values
    
    * feature: add functionality to use other color formats than only full hex values; add minified files
    
    * feature: add functionality to use other color formats than only full hex values; add documentation
    
    * feature: add functionality to use other color formats than only full hex values; use it for empty color, too
    
    * feature: add functionality to use other color formats than only full hex values; newly build files
    
    * feature: add functionality to use other color formats; improve documentation at code
    
    * feature: add functionality to use other color formats; new build

--- src/js/svgMap.js
+++ src/js/svgMap.js
@@ -412,7 +412,7 @@ function svgMapWrapper(svgPanZoom) {
           return;
         }
         if (!data.values[countryID]) {
-          element.setAttribute('fill', this.options.colorNoData);
+          element.setAttribute('fill', this.toHex(this.options.colorNoData));
           return;
         }
         if (typeof data.values[countryID].color != 'undefined') {
@@ -425,8 +425,8 @@ function svgMapWrapper(svgPanZoom) {
         );
         var ratio = Math.max(0, Math.min(1, (value - min) / (max - min)));
         var color = this.getColor(
-          this.options.colorMax,
-          this.options.colorMin,
+          this.toHex(this.options.colorMax),
+          this.toHex(this.options.colorMin),
           ratio || ratio === 0 ? ratio : 1
         );
         element.setAttribute('fill', color);
@@ -2125,6 +2125,33 @@ function svgMapWrapper(svgPanZoom) {
     );
     return '#' + this.getHex(r) + this.getHex(g) + this.getHex(b);
   };
+
+  /**
+   * convert color to hex to allow users of the map to pass in
+   * colors in formats other than full hex value.
+   * @param color
+   * @param element
+   * @returns {string}
+   */
+  svgMap.prototype.toHex = function (color, element = document.documentElement) {
+
+    // Resolve CSS variables
+    if (color.startsWith('var(')) {
+      const cssVar = color.slice(4, -1).trim().replaceAll(/["']+/g, '');
+      color = getComputedStyle(element).getPropertyValue(cssVar).trim();
+    }
+
+    // Create an offscreen canvas
+    const canvas = new OffscreenCanvas(1, 1);
+    const ctx = canvas.getContext('2d');
+
+    // Use canvas to parse the color
+    ctx.fillStyle = color;
+
+    // computed color is full hex code in the case of one color only
+    return ctx.fillStyle
+  }
+
 
   // Get a hex value
 
